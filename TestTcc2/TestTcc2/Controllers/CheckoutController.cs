d6095d0bd55595677f313f2cc1d0d6cba1da4452
pedro henrique nazario da silva <pedrro2@hotmail.com>
2015-05-14 12:51:23 -0300

    FreeCode + adição de música
    Após o usuário completar o preenchimento do checkout e se o valor do
    carrinho for 0 por causa do freecode, a música é adicionada diretamente
    na biblioteca.

--- TestTcc2/TestTcc2/Controllers/CheckoutController.cs
+++ TestTcc2/TestTcc2/Controllers/CheckoutController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using TestTcc2.Models;
 
 namespace TestTcc2.Controllers
@@ -65,10 +66,42 @@ namespace TestTcc2.Controllers
                 o => o.OrderId == id &&
                 o.Username == User.Identity.Name);
 
-           // bool CheckOrderValue = storeDB.Orders.Any(o => o.OrderId == id && o.Total == 0);
+            bool CheckOrderValue = storeDB.Orders.Any(o => o.OrderId == id && o.Total == 0);
 
-            if (isValid)
+            if (isValid && CheckOrderValue)
             {
+                int idUser = 0;
+                if (Request.IsAuthenticated)
+                {
+                    var membership = (WebMatrix.WebData.SimpleMembershipProvider)Membership.Provider;
+                    idUser = membership.GetUserId(User.Identity.Name);
+                }
+
+                List<int> musicaIDs = storeDB.OrderDetails.Where(o => o.OrderId == id).Select(o => o.MusicaId).ToList();
+
+                foreach (var musicaId in musicaIDs)
+                {
+                    var musicas = storeDB.Musicas.Where(x => x.MusicaId == musicaId)
+                        .Select(x => new { x.Nome, x.NomeArtista, x.genero, x.path }).ToList().First();
+
+                    var y = new UsuarioMusica()
+                    {
+                        UserId = idUser,
+                        MusicaId = musicaId,
+                        GeneroId = musicas.genero.GeneroId,
+                        genero = musicas.genero,
+                        Nome = musicas.Nome,
+                        NomeArtista = musicas.NomeArtista,
+                        path = musicas.path
+                    };
+
+                    if (ModelState.IsValid)
+                    {
+                        storeDB.UsuarioMusicas.Add(y);
+                    }
+                }
+
+                storeDB.SaveChanges();
 
                 return View(id);
             }
